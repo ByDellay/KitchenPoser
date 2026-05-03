9477d7b01da0f4511a222210869a8ecf6431fcca
Dellay <tecnicodellaygames@gmail.com>
2026-05-03 14:16:20 -0300

    Botões Play e Quit adicionados

---
+++ KitchenPoser/Assets/Scripts/HUD/ButtonMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]private Button ButtonPlay;
+    [SerializeField]private Button ButtonQuit;
+
+    private void Awake()
+    {
+        ButtonPlay.onClick.AddListener(OnButtonPlayClick);
+        ButtonQuit.onClick.AddListener(OnButtonQuitClick);
+    }
+
+    private void OnButtonPlayClick()
+    {
+        SceneManager.LoadScene("StoryBoard");
+    }
+    private void OnButtonQuitClick()
+    {
+        Debug.Log("Fechando o jogo..."); // Apenas para testar no editor
+        Application.Quit(); // Fecha o executável buildado, não da pra ver direto da unity, mas se voce buildar o executavel >provavelmente funciona<
+    }
+}
