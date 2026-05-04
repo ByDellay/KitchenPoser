147d65a3d40b19bf0056757d0226dfd3e2473701
PatricioJr <bebonnaluz1@gmail.com>
2026-05-04 11:33:17 -0300

    botão storyboard e cena tutorial
    merda ddrmrerrrdrdrddd

---
+++ KitchenPoser/Assets/Scripts/ButtonSkip.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonSkip : MonoBehaviour
+{
+
+    [SerializeField] private Button ButtonSkip01;
+
+
+    private void Awake()
+    {
+        ButtonSkip01.onClick.AddListener(OnButtonSkipClick);
+    }
+
+    private void OnButtonSkipClick()
+    {
+        SceneManager.LoadScene("CortarTutorial");
+    }
+}
--- KitchenPoser/Assets/Scripts/HUD/ButtonMenu.cs
+++ KitchenPoser/Assets/Scripts/HUD/ButtonMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class ButtonMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     [SerializeField]private Button ButtonPlay;
     [SerializeField]private Button ButtonQuit;
 
