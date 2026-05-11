115778fc32fcd91afec1986002342a225ecde01d
PatricioJr <bebonnaluz1@gmail.com>
2026-05-11 16:24:21 -0300

    tentativa de fazer o objeto se mover com o drag
    arrumar isso em casa com o kayles

---
+++ KitchenPoser/Assets/Scripts/Game/DraggableItem.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private CanvasGroup canvasGroup;
+
+    private Transform originalParent;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+
+        // tira do Grid
+        transform.SetParent(canvas.transform);
+
+        // deixa passar raycast
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // volta pro slot original
+        transform.SetParent(originalParent);
+
+        canvasGroup.blocksRaycasts = true;
+    }
+}
