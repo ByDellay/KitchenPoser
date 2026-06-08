6fb6aed01a95aca52a3b3e1c2b1cbf8c7bcc8d14
Qwzz-yt <dellaiinocenciogabriel@gmail.com>
2026-06-08 14:40:14 -0300

    Script de mudar prefabs no spawner quase completo

--- KitchenPoser/Assets/Scripts/Game/Gravidade, cortaveis/IngredientesGrav.cs
+++ KitchenPoser/Assets/Scripts/Game/Gravidade, cortaveis/IngredientesGrav.cs
@@ -4,6 +4,7 @@ public class IngredientesGrav : MonoBehaviour
 {
     // Guarda o Rigidbody2D do objeto
     Rigidbody2D RigidBody;
+     public Alimentos.ItemType Type;
     bool AlreadyCutted = false;
 
     // For�a m�xima que o objeto pode receber pra subir
@@ -87,7 +88,7 @@ public class IngredientesGrav : MonoBehaviour
             {
                 
                 AlreadyCutted = true;
-                GameManager.Instance.AddItem(Alimentos.ItemType.Peixe);
+                GameManager.Instance.AddItem(Type);
                 sr.sprite = SpriteCortado;
                
             }
--- KitchenPoser/Assets/Scripts/Game/Gravidade, cortaveis/Spawner.cs
+++ KitchenPoser/Assets/Scripts/Game/Gravidade, cortaveis/Spawner.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Prefab que será criado
-    public GameObject Ingrediente;
+    // Prefab que serï¿½ criado
+    public GameObject[] Alimentos;
 
     // Tempo entre cada spawn
     public float SpawnTime = 1f;
 
     void Start()
     {
-        // Repete a função Spawnar()
+        // Repete a funï¿½ï¿½o Spawnar()
         // a cada 1 segundo
         InvokeRepeating(nameof(Spawnar), 0f, SpawnTime);
     }
@@ -20,10 +20,12 @@ public class Spawner : MonoBehaviour
         float randomX = Random.Range(-5f, 5f);
 
         Vector3 pos = new Vector3(randomX, transform.position.y, 0);
+        int i = Random.Range(0, Alimentos.Length);
 
-        Instantiate(Ingrediente, pos, Quaternion.identity);
 
-        // Cria o objeto na posição do Spawner
-        Instantiate(Ingrediente, transform.position, Quaternion.identity);
+        Instantiate(Ingrediente[i], pos, Quaternion.identity);
+
+        // Cria o objeto na posiï¿½ï¿½o do Spawner
+        //Instantiate(Ingrediente, transform.position, Quaternion.identity);
     }
 }
