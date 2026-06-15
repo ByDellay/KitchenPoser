05bb2e8b82d357106c5cc46854e66147fe353d0f
Patricio jr <bebonnaluz1@gmail.com>
2026-06-15 16:38:54 -0300

    mudar de pagina QUASE funcionando

--- KitchenPoser/Assets/Scripts/HUD/LivroReceitas.cs
+++ KitchenPoser/Assets/Scripts/HUD/LivroReceitas.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,15 @@ public class LivroReceitas : MonoBehaviour
     public GameObject CookBookOpen;
     public GameObject ButtonOpen;
     public GameObject ButtonClose;
+
+    //public string[] bookPage = { 0, 1, 2, 3 };
+
+    public GameObject pageChangeLeft;
+    public GameObject pageChangeRight;
+    public GameObject page1;
+    public GameObject page2;
+    public GameObject page3;
+
 
 
     public void BookStateClick()
@@ -27,6 +37,70 @@ public class LivroReceitas : MonoBehaviour
             fundoEscuro.SetActive(false);
         }
     }
+
+
+    public void PageChangeRight()
+    {
+        if (page1.activeSelf)
+        {
+            Debug.Log("mudou pagina");
+            page1.SetActive(false);
+            page2.SetActive(true);
+        }
+        else if(page2.activeSelf) 
+        {
+            Debug.Log("mudou pagina");
+            page2.SetActive(false);
+            page3.SetActive(true);
+        }
+    }
+    public void PageChangeLeft()
+    {
+        if (page3.activeSelf)
+        {
+            page3.SetActive(false);
+            page2.SetActive(true);
+        }
+        else if (page2.activeSelf)
+        {
+            page2.SetActive(false);
+            page1.SetActive(true);
+        }
+    }
+    public void Update()
+    {
+        if (page1.activeSelf)
+        {
+            pageChangeLeft.SetActive(false);
+        }
+        else if (page3.activeSelf)
+        {
+            pageChangeRight.SetActive(false);
+        }
+        else
+        {
+            pageChangeLeft.SetActive(true);
+            pageChangeRight.SetActive(true);
+        }
+    }
+
+
+
+
+    
+
+    /*public switch BookPage
+    {
+    case 1 
+    exibir aprimeira pagina
+    desativar todas as outras
+    break
+
+    case 2
+
+    }*/
+
+
 
 
 
