4e5cfe1eadb46c7f6f5e0557428fc06cf357f1b6
Patricio jr <bebonnaluz1@gmail.com>
2026-08-05 15:46:38 -0300

    tentando concertar o spawner quebrado

--- KitchenPoser/Assets/Scripts/Game/Gravidade, cortaveis/Spawner.cs
+++ KitchenPoser/Assets/Scripts/Game/Gravidade, cortaveis/Spawner.cs
@@ -6,8 +6,10 @@ public class Spawner : MonoBehaviour
     public GameObject[] Alimentos;
     public GameObject SpawnerObject;
 
-    // Tempo entre cada spawn
+    //spawn
     public float SpawnTime = 1f;
+    public Transform leftSpawn;
+    public Transform rightSpawn;
 
     public Transform meuPai;
 
@@ -20,11 +22,13 @@ public class Spawner : MonoBehaviour
             InvokeRepeating(nameof(Spawnar), 0f, SpawnTime);
     }
 
+    
+
     void Spawnar()
     {
-        if (SpawnerObject.activeSelf) 
-        {
-            float randomX = Random.Range(-5f, 5f);
+        
+        
+        float randomX = Random.Range(-5f, 5f);
 
         Vector3 pos = new Vector3(randomX, transform.position.y, 0);
         int i = Random.Range(0, Alimentos.Length);
@@ -32,14 +36,21 @@ public class Spawner : MonoBehaviour
 
         Instantiate(Alimentos[i], transform);
         transform.SetParent(meuPai);
-        }
-        else 
-        {
-            
-        }
         
 
         // Cria o objeto na posi��o do Spawner
         //Instantiate(Ingrediente, transform.position, Quaternion.identity);
     }
+
+    /*void Spawnar2()
+    {
+        //instancia o prefab na cena
+        GameObject Comida = Instantiate(Alimentos, transform.position, Quaternion.identity);
+
+        //gera um valor aleatorio e armazena em uma variavel
+        float newX = Random.Range(leftSpawn.position.x, rightSpawn.position.x);
+
+        //atribui a nova posição para o inimigo
+        Comida.transform.position = new Vector2(newX, transform.position.y);
+    } */
 }
