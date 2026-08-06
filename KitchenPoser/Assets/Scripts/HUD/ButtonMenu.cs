50a0faf5e954cfb4a81025224a950c8832ec7d66
Patricio jr <bebonnaluz1@gmail.com>
2026-08-06 13:48:56 -0300

    Creditos incompletos

--- KitchenPoser/Assets/Scripts/HUD/ButtonMenu.cs
+++ KitchenPoser/Assets/Scripts/HUD/ButtonMenu.cs
@@ -4,13 +4,20 @@ using UnityEngine.UI;
 
 public class ButtonMenu : MonoBehaviour
 {
-
+    [Header("Botões")]
     [SerializeField]private Button ButtonPlay;
+    [SerializeField]private Button ButtonCredit;
+    [SerializeField]private Button ButtonCreditClose;
     [SerializeField]private Button ButtonQuit;
+
+    [Header("outro")]
+    [SerializeField] GameObject Creditos;
 
     private void Awake()
     {
         ButtonPlay.onClick.AddListener(OnButtonPlayClick);
+        ButtonCredit.onClick.AddListener(OnButtonCreditClick);
+        ButtonCreditClose.onClick.AddListener(OnButtonCreditCloseClick);
         ButtonQuit.onClick.AddListener(OnButtonQuitClick);
     }
 
@@ -18,6 +25,27 @@ public class ButtonMenu : MonoBehaviour
     {
         SceneManager.LoadScene("StoryBoard");
     }
+    private void OnButtonCreditClick()
+    {
+        Debug.Log("Abrindo os creditos");
+        Creditos.SetActive(true);
+        ButtonCreditClose.gameObject.SetActive(true);
+
+        ButtonPlay.gameObject.SetActive(false);
+        ButtonCredit.gameObject.SetActive(false);
+        ButtonQuit.gameObject.SetActive(false);
+    }
+    private void OnButtonCreditCloseClick()
+    {
+        Debug.Log("Fechando os creditos");
+        Creditos.SetActive(false);
+        ButtonCreditClose.gameObject.SetActive(false);
+
+        ButtonPlay.gameObject.SetActive(true);
+        ButtonCredit.gameObject.SetActive(true);
+        ButtonQuit.gameObject.SetActive(true);
+    }
+
     private void OnButtonQuitClick()
     {
         Debug.Log("Fechando o jogo..."); // Apenas para testar no editor
