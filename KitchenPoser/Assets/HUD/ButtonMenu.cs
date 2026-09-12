6eb2bf75ecc5d1e416ff1ca61454c3e72ebb7215
PatricioJr <bebonnaluz1@gmail.com>
2026-09-12 20:12:51 -0300

    creditos quase prontos e organizando umas coisinhas

--- KitchenPoser/Assets/HUD/ButtonMenu.cs
+++ KitchenPoser/Assets/HUD/ButtonMenu.cs
@@ -5,69 +5,37 @@ using System.Collections;
 
 public class ButtonMenu : MonoBehaviour
 {
-    [Header("Bot�es")]
-    [SerializeField]private Button ButtonPlay;
-    [SerializeField]private Button ButtonCredit;
-    [SerializeField]private Button ButtonCreditClose;
-    [SerializeField]private Button ButtonQuit;
-
-    [Header("outro")]
     [SerializeField] GameObject Creditos;
+    [SerializeField] GameObject Menu;
     [SerializeField] private AudioClip ClickSFX;
 
     //int FadeTime = 1;
     //public Animator CrossFade;
-
-    private void Awake()
-    {
-        ButtonPlay.onClick.AddListener(OnButtonPlayClick);
-        ButtonCredit.onClick.AddListener(OnButtonCreditClick);
-        ButtonCreditClose.onClick.AddListener(OnButtonCreditCloseClick);
-        ButtonQuit.onClick.AddListener(OnButtonQuitClick);
-    }
-
-    private void OnButtonPlayClick()
+    public void OnButtonPlayClick()
     {
         AudioManager.Instance.AudioPlaySFX(ClickSFX, transform, 1f, 1f);
 
         TransitionsFade.Instance.CallCoroutine_LoadNewScene("StoryBoard");
         //SceneManager.LoadScene("StoryBoard");
     }
-    private void OnButtonCreditClick()
+    public void OnButtonCreditClick()
     {
         AudioManager.Instance.AudioPlaySFX(ClickSFX, transform, 1f, 1f);
         Debug.Log("Abrindo os creditos");
         Creditos.SetActive(true);
-        ButtonCreditClose.gameObject.SetActive(true);
-
-        ButtonPlay.gameObject.SetActive(false);
-        ButtonCredit.gameObject.SetActive(false);
-        ButtonQuit.gameObject.SetActive(false);
+        Menu.SetActive(false);
     }
-    private void OnButtonCreditCloseClick()
+    public void OnButtonCreditCloseClick()
     {
         AudioManager.Instance.AudioPlaySFX(ClickSFX, transform, 1f, 1f);
         Debug.Log("Fechando os creditos");
         Creditos.SetActive(false);
-        ButtonCreditClose.gameObject.SetActive(false);
-
-        ButtonPlay.gameObject.SetActive(true);
-        ButtonCredit.gameObject.SetActive(true);
-        ButtonQuit.gameObject.SetActive(true);
+        Menu.SetActive(true);
     }
 
-    private void OnButtonQuitClick()
+    public void OnButtonQuitClick()
     {
         AudioManager.Instance.AudioPlaySFX(ClickSFX, transform, 1f, 1f);
-        Application.Quit(); // Fecha o execut�vel buildado, n�o da pra ver direto da unity, mas se voce buildar o executavel >provavelmente funciona<
+        Application.Quit();
     }
-
-    /*IEnumerator LoadNewScene(string Scene)
-    {
-        CrossFade.SetTrigger("Start");
-
-        yield return new WaitForSeconds(FadeTime);
-
-        SceneManager.LoadScene(Scene);
-    }*/
 }
